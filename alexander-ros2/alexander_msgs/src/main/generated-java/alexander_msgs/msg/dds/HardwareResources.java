07651fefa4f644bf3fa1ddddbd4262af478e1ac0
Stefan Fasano <sfasano@ihmc.org>
2025-08-25 17:41:18 -0500

    added new fields to HardwareResources ROS message

--- alexander-ros2/alexander_msgs/src/main/generated-java/alexander_msgs/msg/dds/HardwareResources.java
+++ alexander-ros2/alexander_msgs/src/main/generated-java/alexander_msgs/msg/dds/HardwareResources.java
@@ -15,15 +15,33 @@ public class HardwareResources extends Packet<HardwareResources> implements Sett
             * Designate the number of xml resources and their names
             */
    public long num_resources_;
+   /**
+            * The directory containing the resources of the desired version of the robot
+            */
+   public java.lang.StringBuilder version_directory_;
+   /**
+            * The list of individual XML files comprising the full XML description of the robot
+            */
    public java.lang.StringBuilder[] xml_resources_;
+   /**
+            * The list of individual URDF files comprising the full URDF description of the robot
+            */
+   public java.lang.StringBuilder[] urdf_resources_;
 
    public HardwareResources()
    {
-      xml_resources_ = new java.lang.StringBuilder[7];
+      version_directory_ = new java.lang.StringBuilder(32);
+      xml_resources_ = new java.lang.StringBuilder[9];
 
       for(int i1 = 0; i1 < xml_resources_.length; ++i1)
       {
           xml_resources_[i1] = new java.lang.StringBuilder();
+      }
+      urdf_resources_ = new java.lang.StringBuilder[6];
+
+      for(int i3 = 0; i3 < urdf_resources_.length; ++i3)
+      {
+          urdf_resources_[i3] = new java.lang.StringBuilder();
       }
    }
 
@@ -37,10 +55,19 @@ public class HardwareResources extends Packet<HardwareResources> implements Sett
    {
       num_resources_ = other.num_resources_;
 
-      for(int i3 = 0; i3 < xml_resources_.length; ++i3)
+      version_directory_.setLength(0);
+      version_directory_.append(other.version_directory_);
+
+      for(int i5 = 0; i5 < xml_resources_.length; ++i5)
       {
-            xml_resources_[i3].setLength(0);
-            xml_resources_[i3].append(other.xml_resources_[i3]);
+            xml_resources_[i5].setLength(0);
+            xml_resources_[i5].append(other.xml_resources_[i5]);
+      }
+
+      for(int i7 = 0; i7 < urdf_resources_.length; ++i7)
+      {
+            urdf_resources_[i7].setLength(0);
+            urdf_resources_[i7].append(other.urdf_resources_[i7]);
       }
 
    }
@@ -60,10 +87,46 @@ public class HardwareResources extends Packet<HardwareResources> implements Sett
       return num_resources_;
    }
 
+   /**
+            * The directory containing the resources of the desired version of the robot
+            */
+   public void setVersionDirectory(java.lang.String version_directory)
+   {
+      version_directory_.setLength(0);
+      version_directory_.append(version_directory);
+   }
 
+   /**
+            * The directory containing the resources of the desired version of the robot
+            */
+   public java.lang.String getVersionDirectoryAsString()
+   {
+      return getVersionDirectory().toString();
+   }
+   /**
+            * The directory containing the resources of the desired version of the robot
+            */
+   public java.lang.StringBuilder getVersionDirectory()
+   {
+      return version_directory_;
+   }
+
+
+   /**
+            * The list of individual XML files comprising the full XML description of the robot
+            */
    public java.lang.StringBuilder[] getXmlResources()
    {
       return xml_resources_;
+   }
+
+
+   /**
+            * The list of individual URDF files comprising the full URDF description of the robot
+            */
+   public java.lang.StringBuilder[] getUrdfResources()
+   {
+      return urdf_resources_;
    }
 
 
@@ -86,9 +149,15 @@ public class HardwareResources extends Packet<HardwareResources> implements Sett
 
       if (!us.ihmc.idl.IDLTools.epsilonEqualsPrimitive(this.num_resources_, other.num_resources_, epsilon)) return false;
 
-      for(int i5 = 0; i5 < xml_resources_.length; ++i5)
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsStringBuilder(this.version_directory_, other.version_directory_, epsilon)) return false;
+
+      for(int i9 = 0; i9 < xml_resources_.length; ++i9)
       {
-                if (!us.ihmc.idl.IDLTools.epsilonEqualsStringBuilder(this.xml_resources_[i5], other.xml_resources_[i5], epsilon)) return false;}
+                if (!us.ihmc.idl.IDLTools.epsilonEqualsStringBuilder(this.xml_resources_[i9], other.xml_resources_[i9], epsilon)) return false;}
+
+      for(int i11 = 0; i11 < urdf_resources_.length; ++i11)
+      {
+                if (!us.ihmc.idl.IDLTools.epsilonEqualsStringBuilder(this.urdf_resources_[i11], other.urdf_resources_[i11], epsilon)) return false;}
 
 
       return true;
@@ -105,9 +174,14 @@ public class HardwareResources extends Packet<HardwareResources> implements Sett
 
       if(this.num_resources_ != otherMyClass.num_resources_) return false;
 
-      for(int i7 = 0; i7 < xml_resources_.length; ++i7)
+      if (!us.ihmc.idl.IDLTools.equals(this.version_directory_, otherMyClass.version_directory_)) return false;
+
+      for(int i13 = 0; i13 < xml_resources_.length; ++i13)
       {
-                if (!us.ihmc.idl.IDLTools.equals(this.xml_resources_[i7], otherMyClass.xml_resources_[i7])) return false;}
+                if (!us.ihmc.idl.IDLTools.equals(this.xml_resources_[i13], otherMyClass.xml_resources_[i13])) return false;}
+      for(int i15 = 0; i15 < urdf_resources_.length; ++i15)
+      {
+                if (!us.ihmc.idl.IDLTools.equals(this.urdf_resources_[i15], otherMyClass.urdf_resources_[i15])) return false;}
 
       return true;
    }
@@ -120,8 +194,12 @@ public class HardwareResources extends Packet<HardwareResources> implements Sett
       builder.append("HardwareResources {");
       builder.append("num_resources=");
       builder.append(this.num_resources_);      builder.append(", ");
+      builder.append("version_directory=");
+      builder.append(this.version_directory_);      builder.append(", ");
       builder.append("xml_resources=");
-      builder.append(java.util.Arrays.toString(this.xml_resources_));
+      builder.append(java.util.Arrays.toString(this.xml_resources_));      builder.append(", ");
+      builder.append("urdf_resources=");
+      builder.append(java.util.Arrays.toString(this.urdf_resources_));
       builder.append("}");
       return builder.toString();
    }
